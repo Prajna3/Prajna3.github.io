e4bcbad6af4d37ac12a0ae64420c20cf672b04c1
Prajna3 <50235971+Prajna3@users.noreply.github.com>
2020-12-24 15:10:29 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system </title>
+  <link rel="stylesheet" type="text/css" href="styled.css">
+  <style type="text/css">
+    html{height:100%;}
+    body{background-image:url('carred.jpg');
+height: 100%;
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+overflow: hidden;}
+button{height: 200px;
+width: 300px;
+color:white;
+text-shadow:2px 2px black;
+font-family:sans-serif;
+font-size: 18px;
+background-color:rgba(107, 124, 150, 0.5);
+border-width:5px;
+border-color:#85adad ;
+border-radius: 10px;}
+</style>
+</head>
+<body>
+	
+</body>
+</html>
